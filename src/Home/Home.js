c90094b1509872c5c58c42898181d1b37a8b7e9a
saran-mani <saranmvdm@gmail.com>
2023-04-30 02:27:12 -0700

    test

---
+++ src/Home/Home.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+class Home extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+            <div>
+                <h1>AI & ML Exercise</h1>
+            </div>
+            </>
+        );
+    }
+}
+ 
+export default Home;
